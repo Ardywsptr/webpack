99658e98f249bba1fab4146ddc449cf90171d17e
Ardy wirasaputra <ardywsptr1@gmail.com>
2023-07-17 14:12:47 +0700

    update loaders
    menambahkan loader style-loader, css-loader, babel-loader, sass-loader, dan
     menerapkan bootstrap bundling.

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,31 @@ module.exports = {
     },
     mode: 'development',
     watch: true,
-    devtool: false
+    devtool: false,
+    module: {
+        rules: [{
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env'],
+                        ],
+                    },
+                },
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+        ]
+    },
 };

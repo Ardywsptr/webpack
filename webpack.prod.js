22707bb44c593fe0783d0c61cfc5ef142d93ef53
Ardy wirasaputra <ardywsptr1@gmail.com>
2023-07-21 00:18:26 +0700

    update plugin sharp

--- webpack.prod.js
+++ webpack.prod.js
@@ -9,6 +9,7 @@ const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const {
     PurgeCSSPlugin
 } = require("purgecss-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 
 const PATHS = {
     src: path.join(__dirname, "src"),
@@ -41,6 +42,11 @@ module.exports = merge(common, {
             // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
             `...`,
             new CssMinimizerPlugin(),
+            new ImageMinimizerPlugin({
+                minimizer: {
+                    implementation: ImageMinimizerPlugin.sharpMinify,
+                },
+            }),
         ],
     },
     plugins: [
